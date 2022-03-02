48e574abf41cccf50f90aec79f0ba59a08ba542d
rere-dnaw <rafal.wilk92@gmail.com>
2022-03-02 15:08:03 +0100

    implemented bootstrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dash import dcc, html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import chart_burn_DC
@@ -14,43 +15,44 @@ fig_inflation = go.Figure()
 chart_inflation.create_chart_inflation(fig_inflation)
 
 
-app.layout = html.Div([
-    html.H1('Helium data', className='title_style'),
-    dcc.Tabs(
-        id="tabs_component",
-        value='tab_burn_DC',
-        parent_className='custom-tabs',
-        className='custom-tabs-container',
-        children=[
-            dcc.Tab(
-                label='Burned DC',
-                value='tab_burn_DC',
-                className='custom-tab',
-                selected_className='custom-tab--selected',
-            ),
-            dcc.Tab(
-                label='Inflation-Burn',
-                value='tab_burn_infla',
-                className='custom-tab',
-                selected_className='custom-tab--selected',
-            ),
-        ]),
-    html.Div(id='tabs-content-example-graph', className='div_test')
-], className='div_test')
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("HELIUM DATA",
+                        className='text-center text-white align-text-bottom m-5 font-weight-bold'),
+                width=12)
+    ]),
 
-@app.callback(Output('tabs-content-example-graph', 'children'),
-              Input('tabs_component', 'value'))
+    dbc.Row([
+        dbc.Col([
+            dbc.Tabs(
+                [
+                    dbc.Tab(label='Burned DC',tab_id='tab_burn_DC'),
+                    dbc.Tab(label='Inflation-Burn',tab_id='tab_burn_infla'),
+                ],
+                id="tabs",
+                active_tab='tab_burn_DC',
+            ),
+            html.Div(id='tabs-content')
+        ],
+        width={'size':12},
+        )
+    ], justify='center')
+], fluid=True)
+
+
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'active_tab'))
 def render_content(tab):
     if tab == 'tab_burn_DC':
         return html.Div([
-            dcc.Graph(id='example,',
+            dcc.Graph(id='graph-1-tab',
             figure=fig_burnDC,
             style={'height': '80vh'})
         ])
     elif tab == 'tab_burn_infla':
         return html.Div([
             dcc.Graph(
-                id='graph-2-tabs',
+                id='graph-2-tab',
                 figure=fig_inflation,
                 style={'height': '80vh'})
         ])
